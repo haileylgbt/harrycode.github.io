07a6eb4aa918b42a9aba6f23584fb657399f2fb5
Harry <pacboy2004@gmail.com>
2020-02-14 10:06:43 +1000

    Add files via upload

---
+++ console.js
@@ -0,0 +1,103 @@
+var inputDest = "";
+var code = [];
+
+function getRandomInt(min, max) {
+    max++;
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function remove(element) {
+    element.parentNode.removeChild(element);
+}
+
+function clear() {
+    while (document.getElementsByClassName("code") !== undefined) {
+        this.elems = document.getElementsByClassName("code");
+        for (var i in this.elems) {
+            remove(this.elems[0]);
+        }
+    }
+}
+
+function print(text) {
+    this.p = document.createElement("p");
+    this.p.innerText = text;
+    this.p.className = "code";
+    this.br = document.createElement("br");
+    this.br.className = "code";
+    document.body.appendChild(this.p);
+}
+
+
+function newCommand(name, func) {
+    if (this.args[0] == name) {
+        eval(func);
+    }
+}
+
+function printMulti(list) {
+    for (var i in list) {
+        print(list[i].toString());
+    }
+}
+
+
+function runLine(code) {
+    this.args = code.split(" ");
+    if(this.args[this.args.length-1] != ","){
+
+        newCommand("clear", "clear()")
+        newCommand("milliunix", "print(Date.now())")
+        newCommand("unix", "print(Math.round(Date.now() / 1000))")
+        newCommand("date", "print(moment().format('MMMM Do YYYY'))")
+        newCommand("rand", "print(getRandomInt(this.args[1], this.args[2]))")
+        newCommand("exit", "history.go(-1)")
+        newCommand("sitewarp", "window.location.href = this.args[1] + '.html'")
+        newCommand("help", "print('Showing help:\nclear - clears the terminal.\nunix - shows how long its been since January 1, 1970 (midnight) in seconds.\nmilliunix - same as unix but in milliseconds\ndate - shows the current date.\nrand <min> <max> - currently broken.\nexit - sends you back to the website.\nsitewarp <page> - can send you to any page on the website (if you know what the html file for it is called).')")
+
+        
+    }else{
+      [code.length] = this.args.slice(0, this.args.length-1).join(" ");
+
+        
+    }
+}
+
+function runCode(code) {
+    for (var i in this.lines){
+      runLine(this.lines[i]);
+    }
+}
+
+function createinput(prompt) {
+    this.input = document.createElement("input");
+    this.input.type = "text";
+    this.input.id = "input";
+    this.input.className = "code";
+    this.input.autofocus = true;
+    this.input.size = 115;
+    this.input.maxLength = 115;
+    this.input.placeholder = prompt;
+    this.input.onblur = function() {
+        document.getElementById("input").focus();
+    };
+    this.input.onchange = function() {
+        inputDest = document.getElementById("input").value;
+        remove(document.getElementById("input"));
+        print(inputDest);
+        this.data = runLine(inputDest);
+        if(this.data !== undefined){
+          print(this.data);
+        }
+    };
+    document.body.appendChild(this.input);
+}
+
+printMulti(["Loaded version 1.0 'Harriet'"]);
+
+window.setInterval(function() {
+    if (document.getElementById("input") === null) {
+        createinput("");
+    }
+    document.getElementById("input").focus();
+}, 100);
